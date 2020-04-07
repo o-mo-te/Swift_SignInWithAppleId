01b629c6870ac50f00a6cf5f1210ae266f483406
hidetomo117 <hidetomo117@gmail.com>
2020-04-07 10:21:18 +0900

    create project

---
+++ Swift_SignInWithAppleId/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_SignInWithAppleId
+//
+//  Created by hidetomo on 2020/04/07.
+//  Copyright © 2020 Hidetomo Masuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
